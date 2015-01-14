kernelorg-mirror/linux_kernel_git_ak_linux-misc

perf inject: Cut Instruction Tracing samples
After decoding Instruction Tracing samples, the
Instruction Tracing data is no longer needed
(having been replaced by synthesized events)
so cut it out.

Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

tools/perf/builtin-inject.c
@@ -31,6 +31,7 @@ struct perf_inject {
 	u64			bytes_written;
 	struct list_head	samples;
 	struct itrace_synth_opts itrace_synth_opts;
+	char		 event_copy[PERF_SAMPLE_MAX_SIZE];
 };
 
 struct event_entry {
@@ -138,6 +139,28 @@ static int perf_event__repipe(struct perf_tool *tool,
 	return perf_event__repipe_synth(tool, event);
 }
 
+static union perf_event *
+perf_inject__cut_itrace_sample(struct perf_inject *inject,
+			       union perf_event *event,
+			       struct perf_sample *sample)
+{
+	size_t sz1 = sample->itrace_sample.data - (void *)event;
+	size_t sz2 = event->header.size - sample->itrace_sample.size - sz1;
+	union perf_event *ev = (union perf_event *)inject->event_copy;
+
+	if (sz1 > event->header.size || sz2 > event->header.size ||
+	    sz1 + sz2 > event->header.size ||
+	    sz1 < sizeof(struct perf_event_header) + sizeof(u64))
+		return event;
+
+	memcpy(ev, event, sz1);
+	memcpy((void *)ev + sz1, (void *)event + event->header.size - sz2, sz2);
+	ev->header.size = sz1 + sz2;
+	((u64 *)((void *)ev + sz1))[-1] = 0;
+
+	return ev;
+}
+
 typedef int (*inject_handler)(struct perf_tool *tool,
 			      union perf_event *event,
 			      struct perf_sample *sample,
@@ -150,12 +173,18 @@ static int perf_event__repipe_sample(struct perf_tool *tool,
 				     struct perf_evsel *evsel,
 				     struct machine *machine)
 {
+	struct perf_inject *inject = container_of(tool, struct perf_inject,
+						  tool);
+
 	if (evsel->handler) {
 		inject_handler f = evsel->handler;
 		return f(tool, event, sample, evsel, machine);
 	}
 
 	build_id__mark_dso_hit(tool, event, sample, evsel, machine);
+
+	if (inject->itrace_synth_opts.set && sample->itrace_sample.size)
+		event = perf_inject__cut_itrace_sample(inject, event, sample);
 
 	return perf_event__repipe_synth(tool, event);
 }

liwenhaosuper/linux-perf

tools/perf/builtin-inject.c
@@ -33,6 +33,7 @@ struct perf_inject {
 	u64			bytes_written;
 	struct list_head	samples;
 	struct itrace_synth_opts itrace_synth_opts;
+	char		 event_copy[PERF_SAMPLE_MAX_SIZE];
 };
 
 struct event_entry {
@@ -154,6 +155,28 @@ static int perf_event__repipe(struct perf_tool *tool,
 	return perf_event__repipe_synth(tool, event);
 }
 
+static union perf_event *
+perf_inject__cut_itrace_sample(struct perf_inject *inject,
+			       union perf_event *event,
+			       struct perf_sample *sample)
+{
+	size_t sz1 = sample->aux_sample.data - (void *)event;
+	size_t sz2 = event->header.size - sample->aux_sample.size - sz1;
+	union perf_event *ev = (union perf_event *)inject->event_copy;
+
+	if (sz1 > event->header.size || sz2 > event->header.size ||
+	    sz1 + sz2 > event->header.size ||
+	    sz1 < sizeof(struct perf_event_header) + sizeof(u64))
+		return event;
+
+	memcpy(ev, event, sz1);
+	memcpy((void *)ev + sz1, (void *)event + event->header.size - sz2, sz2);
+	ev->header.size = sz1 + sz2;
+	((u64 *)((void *)ev + sz1))[-1] = 0;
+
+	return ev;
+}
+
 typedef int (*inject_handler)(struct perf_tool *tool,
 			      union perf_event *event,
 			      struct perf_sample *sample,
@@ -166,12 +189,18 @@ static int perf_event__repipe_sample(struct perf_tool *tool,
 				     struct perf_evsel *evsel,
 				     struct machine *machine)
 {
+	struct perf_inject *inject = container_of(tool, struct perf_inject,
+						  tool);
+
 	if (evsel->handler) {
 		inject_handler f = evsel->handler;
 		return f(tool, event, sample, evsel, machine);
 	}
 
 	build_id__mark_dso_hit(tool, event, sample, evsel, machine);
+
+	if (inject->itrace_synth_opts.set && sample->aux_sample.size)
+		event = perf_inject__cut_itrace_sample(inject, event, sample);
 
 	return perf_event__repipe_synth(tool, event);
 }